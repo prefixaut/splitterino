cc3f70947b3efd85bb79f67d78dbf1176c2bbec3
PreFiXAUT <mail@prefix.moe>
2018-09-24 21:54:31 +0200

    Fixed formatting and added missing dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,58 +1,43 @@
 // https://eslint.org/docs/user-guide/configuring
 
 module.exports = {
-  root: true,
+    root: true,
 
-  parserOptions: {
-    parser: 'typescript-eslint-parser'
-  },
+    parserOptions: {
+        parser: 'typescript-eslint-parser'
+    },
 
-  env: {
-    browser: true,
-  },
+    env: {
+        browser: true
+    },
 
-  extends: [
-    'plugin:vue/essential'
-  ],
+    extends: ['plugin:vue/essential'],
 
-  // required to lint *.vue files
-  plugins: [
-    'vue',
-    'prettier'
-  ],
+    // required to lint *.vue files
+    plugins: ['vue', 'prettier'],
 
-  // add your custom rules here
-  rules: {
-    code: 80,
-    // allow async-await
-    'generator-star-spacing': 'off',
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-console': [
-      'warn'
-    ],
-    indent: 'off',
-    'linebreak-style': [
-      'off'
-    ],
-    quotes: [
-      'error',
-      'single',
-      {
-        avoidEscape: true,
-        allowTemplateLiterals: true
-      }
-    ],
-    semi: [
-      'error',
-      'always'
-    ],
-    'space-before-function-paren': 0,
-    'comma-dangle': 'off'
-  },
+    // add your custom rules here
+    rules: {
+        code: 80,
+        // allow async-await
+        'generator-star-spacing': 'off',
+        // allow debugger during development
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-console': ['warn'],
+        indent: 'off',
+        'linebreak-style': ['off'],
+        quotes: [
+            'error',
+            'single',
+            {
+                avoidEscape: true,
+                allowTemplateLiterals: true
+            }
+        ],
+        semi: ['error', 'always'],
+        'space-before-function-paren': 0,
+        'comma-dangle': 'off'
+    },
 
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/typescript'
-  ]
-}
+    extends: ['plugin:vue/essential', '@vue/typescript']
+};
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,19 @@
 import Vue from 'vue';
 import App from './App.vue';
 
+import ConfigurationEditor from './components/ConfigurationEditor.vue';
+import NumberInput from './components/NumberInput.vue';
+import Splits from './components/Splits.vue';
+import SplitsEditor from './components/SplitsEditor.vue';
+import TimeInput from './components/TimeInput.vue';
+
+// Register Components
+Vue.component('spl-configuiration-editor', ConfigurationEditor);
+Vue.component('spl-number-input', NumberInput);
+Vue.component('spl-splits', Splits);
+Vue.component('spl-splits-editor', SplitsEditor);
+Vue.component('spl-time-input', TimeInput);
+
 Vue.config.productionTip = false;
 
 new Vue({
