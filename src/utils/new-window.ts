8ed87891a6bdb7f3c12493e884eac4571405dd80
PreFiXAUT <mail@prefix.moe>
2018-11-21 21:43:54 +0100

    Added new-window util function

---
+++ src/utils/new-window.ts
@@ -0,0 +1,22 @@
+import { remote, BrowserWindowConstructorOptions } from 'electron';
+
+const defSettings: BrowserWindowConstructorOptions = {
+    webPreferences: {
+        webSecurity: false
+    }
+};
+
+const url = 'http://localhost:8080#';
+
+export function newWindow(
+    settings: BrowserWindowConstructorOptions,
+    route: string = ''
+) {
+    const win = new remote.BrowserWindow({
+        ...defSettings,
+        ...settings
+    });
+    if (!process.env.IS_TEST) win.webContents.openDevTools({ mode: 'detach' });
+    win.loadURL(url + route);
+    win.show();
+}
