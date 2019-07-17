0e91ac5d04a409fad5ef922eff3fb0390a0b3fa9
PreFiXAUT <mail@prefix.moe>
2019-07-17 14:36:47 +0200

    Cleaned up game info module

--- src/store/modules/game-info.module.ts
+++ src/store/modules/game-info.module.ts
@@ -29,16 +29,6 @@ export const ACTION_SET_LANGUAGE = `${MODULE_PATH}/${ID_ACTION_SET_LANGUAGE}`;
 export const ACTION_SET_PLATFORM = `${MODULE_PATH}/${ID_ACTION_SET_PLATFORM}`;
 export const ACTION_SET_REGION = `${MODULE_PATH}/${ID_ACTION_SET_REGION}`;
 
-const moduleState: GameInfoState = {
-    name: null,
-    category: null,
-    language: null,
-    platform: null,
-    region: null,
-};
-
-const getters = {};
-
 const allRegions: Region[] = [
     Region.NTSC,
     Region.NTSC_JPN,
@@ -49,95 +39,101 @@ const allRegions: Region[] = [
     Region.PAL_EUR,
 ];
 
-const mutations = {
-    [ID_MUTATION_SET_GAME_NAME](state: GameInfoState, name: string) {
-        if (typeof name !== 'string') {
-            return;
-        }
+export function getGameInfoStoreModule(): Module<GameInfoState, RootState> {
+    return {
+        namespaced: true,
+        state: {
+            name: null,
+            category: null,
+            language: null,
+            platform: null,
+            region: null,
+        },
+        getters: {},
+        mutations: {
+            [ID_MUTATION_SET_GAME_NAME](state: GameInfoState, name: string) {
+                if (typeof name !== 'string') {
+                    return;
+                }
 
-        state.name = name;
-    },
-    [ID_MUTATION_SET_CATEGORY](state: GameInfoState, category: string) {
-        if (typeof name !== 'string') {
-            return;
-        }
+                state.name = name;
+            },
+            [ID_MUTATION_SET_CATEGORY](state: GameInfoState, category: string) {
+                if (typeof name !== 'string') {
+                    return;
+                }
 
-        state.category = category;
-    },
-    [ID_MUTATION_SET_LANGUAGE](state: GameInfoState, language: string) {
-        if (typeof name !== 'string') {
-            return;
-        }
+                state.category = category;
+            },
+            [ID_MUTATION_SET_LANGUAGE](state: GameInfoState, language: string) {
+                if (typeof name !== 'string') {
+                    return;
+                }
 
-        state.language = language;
-    },
-    [ID_MUTATION_SET_PLATFORM](state: GameInfoState, platform: string) {
-        if (typeof name !== 'string') {
-            return;
-        }
+                state.language = language;
+            },
+            [ID_MUTATION_SET_PLATFORM](state: GameInfoState, platform: string) {
+                if (typeof name !== 'string') {
+                    return;
+                }
 
-        state.platform = platform;
-    },
-    [ID_MUTATION_SET_REGION](state: GameInfoState, region: Region) {
-        if (!allRegions.includes(region)) {
-            return;
-        }
+                state.platform = platform;
+            },
+            [ID_MUTATION_SET_REGION](state: GameInfoState, region: Region) {
+                if (!allRegions.includes(region)) {
+                    return;
+                }
 
-        state.region = region;
-    },
-};
+                state.region = region;
+            },
+        },
+        actions: {
+            async [ID_ACTION_SET_GAME_NAME](context: ActionContext<GameInfoState, RootState>, name: string) {
+                if (typeof name !== 'string') {
+                    return false;
+                }
 
-const actions = {
-    async [ID_ACTION_SET_GAME_NAME](context: ActionContext<GameInfoState, RootState>, name: string) {
-        if (typeof name !== 'string') {
-            return false;
-        }
+                context.commit(ID_MUTATION_SET_GAME_NAME, name);
 
-        context.commit(ID_MUTATION_SET_GAME_NAME, name);
+                return true;
+            },
+            async [ID_ACTION_SET_CATEGORY](context: ActionContext<GameInfoState, RootState>, category: string) {
+                if (typeof name !== 'string') {
+                    return false;
+                }
 
-        return true;
-    },
-    async [ID_ACTION_SET_CATEGORY](context: ActionContext<GameInfoState, RootState>, category: string) {
-        if (typeof name !== 'string') {
-            return false;
-        }
+                context.commit(ID_MUTATION_SET_CATEGORY, category);
 
-        context.commit(ID_MUTATION_SET_CATEGORY, category);
+                return true;
+            },
+            async [ID_ACTION_SET_LANGUAGE](context: ActionContext<GameInfoState, RootState>, language: string) {
+                if (typeof name !== 'string') {
+                    return false;
+                }
 
-        return true;
-    },
-    async [ID_ACTION_SET_LANGUAGE](context: ActionContext<GameInfoState, RootState>, language: string) {
-        if (typeof name !== 'string') {
-            return false;
-        }
+                context.commit(ID_MUTATION_SET_LANGUAGE, language);
 
-        context.commit(ID_MUTATION_SET_LANGUAGE, language);
+                return true;
+            },
+            async [ID_ACTION_SET_PLATFORM](context: ActionContext<GameInfoState, RootState>, platform: string) {
+                if (typeof name !== 'string') {
+                    return false;
+                }
 
-        return true;
-    },
-    async [ID_ACTION_SET_PLATFORM](context: ActionContext<GameInfoState, RootState>, platform: string) {
-        if (typeof name !== 'string') {
-            return false;
-        }
+                context.commit(ID_MUTATION_SET_PLATFORM, platform);
 
-        context.commit(ID_MUTATION_SET_PLATFORM, platform);
+                return true;
+            },
+            async [ID_ACTION_SET_REGION](context: ActionContext<GameInfoState, RootState>, region: Region) {
+                if (!allRegions.includes(region)) {
+                    return false;
+                }
 
-        return true;
-    },
-    async [ID_ACTION_SET_REGION](context: ActionContext<GameInfoState, RootState>, region: Region) {
-        if (!allRegions.includes(region)) {
-            return false;
-        }
+                context.commit(ID_MUTATION_SET_REGION, region);
 
-        context.commit(ID_MUTATION_SET_REGION, region);
+                return true;
+            },
+        },
+    };
+}
 
-        return true;
-    },
-};
-
-export const gameInfoStoreModule: Module<GameInfoState, RootState> = {
-    state: moduleState,
-    getters,
-    mutations,
-    actions,
-};
