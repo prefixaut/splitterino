deb1217964e42554cd443b9a3301c06bf35a74b8
PreFiXAUT <mail@prefix.moe>
2019-10-09 22:14:50 +0200

    Updated build configurations

--- ci/build-docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-docker run --rm \
-    --env-file <(env | grep -vE '\r|\n' | grep -iE 'DEBUG|NODE_|ELECTRON_|YARN_|NPM_|CI|CIRCLE|TRAVIS|APPVEYOR_|CSC_|_TOKEN|_KEY|AWS_|STRIP|BUILD_') \
-    -v ${PWD}:/project \
-    -v ~/.cache/electron:/root/.cache/electron \
-    -v ~/.cache/electron-builder:/root/.cache/electron-builder \
-    electronuserland/builder:wine \
-    /bin/bash -c "yarn install --link-duplicates --pure-lockfile && yarn test:unit && yarn build --publish=onTag --linux --win"
